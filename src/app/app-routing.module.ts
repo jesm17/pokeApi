b6af66896aba29201d5d2ef041aee4a521cc9d79
jesm17 <sanchez17082003@gmail.com>
2022-09-18 22:50:17 -0500

    primera version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AdivinaPokemonComponent } from './components/adivina-pokemon/adivina-pokemon.component';
+import { ItemsComponent } from './components/items/items.component';
+import { NavComponent } from './components/nav/nav.component';
 
-const routes: Routes = [];
+import { PokemonsComponent } from './components/pokemons/pokemons.component';
+
+const routes: Routes = [
+
+  {
+    path: 'adivinaPokemon',
+    component: AdivinaPokemonComponent
+  },
+  {
+    path: 'home',
+    component: PokemonsComponent
+  },
+  {
+    path: '',
+    component:PokemonsComponent
+  },{
+    path:'items',
+    component: ItemsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
